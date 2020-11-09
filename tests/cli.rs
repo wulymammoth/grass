71ae265eb9f229873504831a68bdc4b5fd4523ea
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-11-08 19:23:23 -0800

    Add integration tests
    - integration b/w the library and the actual application that uses it

---
+++ tests/cli.rs
@@ -0,0 +1,27 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for assertions
+use std::io::Write;
+use std::process::Command; // Run programs
+use tempfile::NamedTempFile;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grass")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert().failure().stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grass")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert().success().stdout(predicate::str::contains("test\nAnother test"));
+
+    Ok(())
+}
