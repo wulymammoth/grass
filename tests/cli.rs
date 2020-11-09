c394ae1efdc7762d4a183b6d4e4274e4098edd2b
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-11-08 19:55:31 -0800

    Add integration test
    - scenarios
      - just an empty string

--- tests/cli.rs
+++ tests/cli.rs
@@ -9,7 +9,9 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("grass")?;
 
     cmd.arg("foobar").arg("test/file/doesnt/exist");
-    cmd.assert().failure().stderr(predicate::str::contains("No such file or directory"));
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
 
     Ok(())
 }
@@ -21,7 +23,23 @@ fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut cmd = Command::cargo_bin("grass")?;
     cmd.arg("test").arg(file.path());
-    cmd.assert().success().stdout(predicate::str::contains("test\nAnother test"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
+
+    Ok(())
+}
+
+#[test]
+fn test_empty_string_pattern() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "foo bar baz")?;
+
+    let mut cmd = Command::cargo_bin("grass")?;
+    cmd.arg("").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("baz"));
 
     Ok(())
 }
