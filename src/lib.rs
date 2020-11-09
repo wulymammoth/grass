4b6a0694bafa0f7176ce690dc1df5db90256ac03
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-11-08 19:55:00 -0800

    Extend unit tests
    - scenarios
      - unfound pattern
      - empty string

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
+use core::str;
+
 pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
             match writeln!(writer, "{}", line) {
                 Ok(ok) => ok,
-                Err(err) => panic!(err)
+                Err(err) => panic!(err),
             }
         }
     }
 }
-
 
 #[test]
 fn find_a_match() {
@@ -16,4 +17,20 @@ fn find_a_match() {
     find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut writer);
     // `b` prefix makes the string a byte string literal `&[u8]` instead of `&str`
     assert_eq!(writer, b"lorem ipsum\n");
+}
+
+#[test]
+fn unfound_pattern() {
+    let mut buffer = Vec::new();
+    find_matches("foobar", "\n", &mut buffer);
+    let lhs = str::from_utf8(&buffer).unwrap();
+    assert_eq!(lhs, "");
+}
+
+#[test]
+fn empty_string_pattern() {
+    let mut buffer = Vec::new();
+    find_matches("foobar", "", &mut buffer);
+    let lhs = str::from_utf8(&buffer).unwrap();
+    assert_eq!(lhs, "foobar\n");
 }
